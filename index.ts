a00784689ec1d5eefdb90c9fd328e44fbc5be164
felipeselau <felipeselau68@gmail.com>
2024-05-21 08:43:08 -0300

    implementada uma função para copiar os arquivos das pastas 'sections' e 'snippets'

--- index.ts
+++ index.ts
@@ -4,55 +4,27 @@ const fs = require("fs");
 const path = require("path");
 
 const folderName = process.env.TARGET_FOLDER;
-const folderPath = `./${folderName}`;
+const rootPath = `./${folderName}`;
 
-console.log(`Pasta Raíz: ${folderPath}`);
+console.log(`Pasta Raíz: ${rootPath}`);
 
-if (!fs.existsSync(folderPath)) {
+if (!fs.existsSync(rootPath)) {
   throw new Error("Pasta Raíz não encontrada");
 }
 
-// copia os arquivos da pasta sections
-const sectionFolder = fs.readdirSync(`${folderPath}/sections`);
-if (!fs.existsSync("./src/sections")) {
-  fs.mkdirSync("./src/sections");
-}
-sectionFolder.forEach((file) => {
-  const filePath = path.resolve(`${folderPath}/sections/${file}`);
-  const fileName = file.split(".")[0];
-  let targetFolderPath = `./src/sections/${fileName}`;
-
-  if (!fs.existsSync(targetFolderPath)) {
-    fs.mkdirSync(targetFolderPath);
-  }
-  fs.copyFileSync(filePath, `${targetFolderPath}/${file}`);
-});
-
-// copia os arquivos da pasta snippets
-const snippetsFolder = fs.readdirSync(`${folderPath}/snippets`);
-if (!fs.existsSync("./src/snippets")) {
-  fs.mkdirSync("./src/snippets");
-}
-snippetsFolder.forEach((file) => {
-  const filePath = path.resolve(`${folderPath}/snippets/${file}`);
-  const fileName = file.split(".")[0];
-  let targetFolderPath = `./src/snippets/${fileName}`;
-
-  if (!fs.existsSync(targetFolderPath)) {
-    fs.mkdirSync(targetFolderPath);
-  }
-  fs.copyFileSync(filePath, `${targetFolderPath}/${file}`);
-});
+// copia os arquivos da pasta sections e snippets
+copyContentsOfFolder(rootPath, "sections");
+copyContentsOfFolder(rootPath, "snippets");
 
 // copia os assets para suas respectivas pastas
 // arquivo começando com 'component' -> snippets
 // arquivo começando com 'section' -> sections
-const assetsFolder = fs.readdirSync(`${folderPath}/assets`);
+const assetsFolder = fs.readdirSync(`${rootPath}/assets`);
 if (!fs.existsSync("./src/assets")) {
   fs.mkdirSync("./src/assets");
 }
 assetsFolder.forEach((file) => {
-  const filePath = path.resolve(`${folderPath}/assets/${file}`);
+  const filePath = path.resolve(`${rootPath}/assets/${file}`);
   const fileName = file.split(".")[0];
   const prefix = fileName.split("-")[0];
   const sufix = fileName.substring(prefix.length + 1);
@@ -80,3 +52,23 @@ assetsFolder.forEach((file) => {
 
   fs.copyFileSync(filePath, `${targetFolderPath}/${file}`);
 });
+console.log("Pasta 'assets' copiada com sucesso");
+
+function copyContentsOfFolder(rootFolderPath, sourceFolderName){
+    const selectedFolder = fs.readdirSync(`${rootFolderPath}/${sourceFolderName}`);
+    if (!fs.existsSync(`./src/${sourceFolderName}`)) {
+      fs.mkdirSync(`./src/${sourceFolderName}`);
+    }
+
+    selectedFolder.forEach((file) => {
+      const filePath = path.resolve(`${rootFolderPath}/${sourceFolderName}/${file}`);
+      const fileName = file.split(".")[0];
+      let targetFolderPath = `./src/${sourceFolderName}/${fileName}`;
+
+      if (!fs.existsSync(targetFolderPath)) {
+        fs.mkdirSync(targetFolderPath);
+      }
+      fs.copyFileSync(filePath, `${targetFolderPath}/${file}`);
+    });
+    console.log(`Pasta '${sourceFolderName}' copiada com sucesso`);
+}
